00b0b809964abc400083171c6b79005f9d8ff9a1
Hubariev <yevhenii.hubariev@gmail.com>
2019-07-05 23:05:26 +0200

    Dodanie scryptu jQuery do maximized photo of project 1

---
+++ Port_test/Scripts/bigger-photo.js
@@ -0,0 +1,32 @@
+﻿
+$(function () {
+    $('.minimized').click(function (event) {
+
+        //attr - bierze wartość atrybutu (link do zdjęcia)
+        var i_path = $(this).attr('src');
+
+        //append - wkleja w koniec 'body' kod
+        $('body').append('<div id="overlay"></div><div id="magnify"><img src="' + i_path + '"><div id="close-popup"><i></i></div></div>');
+        $('#magnify').css({
+            left: ($(document).width() - $('#magnify').outerWidth()) / 2,
+            // top: ($(document).height() - $('#magnify').outerHeight())/2 upd: 24.10.2016
+            top: ($(window).height() - $('#magnify').outerHeight()) / 2
+        });
+        //fadeIn - wybrane elementy będą niewidoczne fast - szybko
+        $('#overlay, #magnify').fadeIn('fast');
+
+    });
+
+    $('body').on('click', '#close-popup, #overlay', function (event) {
+        event.preventDefault();
+
+        $('#overlay, #magnify').fadeOut('fast', function () {
+            $('#close-popup, #magnify, #overlay').remove();
+        });
+
+        $('#magnify').css({
+            left: 0,
+            top: 0
+        });
+    });
+});
---
+++ Port_test/Scripts/max-min-photo.js
@@ -0,0 +1,39 @@
+﻿
+$(function () {
+    $('.minimized').click(function (event) {
+
+         //attr - bierze wartość atrybutu (link do zdjęcia)
+        var i_path = $(this).attr('src');
+
+        var left_ = $('#magnify').outerWidth();
+        var top_ = $('#magnify').outerHeight();
+
+        //append - wkleja w koniec 'body' kod
+        $('body').append('<div id="overlay"></div><div id="magnify"><img src="' + i_path + '"><div id="close-popup"><i></i></div></div>');
+        $('#magnify').css({
+            left: ($(document).width() - $('#magnify').outerWidth()) / 2,
+            top: ($(window).height() - $('#magnify').outerHeight()) / 2
+
+            
+
+            //left: $(document).width() / 2,
+            //top: $(window).height() / 2 
+        });
+
+         //fadeIn - wybrane elementy będą niewidoczne fast - szybko
+        $('#overlay, #magnify').fadeIn('fast');
+
+    });
+
+    
+
+    $('body').on('click', '#close-popup, #overlay', function (event) {
+        event.preventDefault();
+
+        $('#overlay, #magnify').fadeOut('fast', function () {
+            //$('#close-popup, #magnify, #overlay').remove();
+        });
+    });
+    alert(left_);
+    alert(top_);
+});
