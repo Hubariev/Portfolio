7b3ab018a8cfc6f599dacf48d4318ed3bc1fa8ef
Hubariev <yevhenii.hubariev@gmail.com>
2019-07-07 16:13:36 +0200

    Działający scrypt do otwierania photo, sekcja projects

--- Port_test/Scripts/bigger-photo.js
+++ Port_test/Scripts/bigger-photo.js
@@ -6,7 +6,7 @@ $(function () {
         var i_path = $(this).attr('src');
 
         //append - wkleja w koniec 'body' kod
-        $('body').append('<div id="overlay"></div><div id="magnify"><img src="' + i_path + '"><div id="close-popup"><i></i></div></div>');
+        $('body').append('<div id="wrap-overlay"><div id="overlay"></div><div id="magnify"><img src="' + i_path + '"><div id="close-popup"><i></i></div></div></div>');
         $('#magnify').css({
             left: ($(document).width() - $('#magnify').outerWidth()) / 2,
             // top: ($(document).height() - $('#magnify').outerHeight())/2 upd: 24.10.2016
@@ -24,9 +24,6 @@ $(function () {
             $('#close-popup, #magnify, #overlay').remove();
         });
 
-        $('#magnify').css({
-            left: 0,
-            top: 0
-        });
+        
     });
 });
--- Port_test/Scripts/max-min-photo.js
+++ Port_test/Scripts/max-min-photo.js
@@ -1,42 +1,25 @@
 ﻿
 $(function () {
-   
-
     $('.minimized').click(function (event) {
 
-         //attr - bierze wartość atrybutu (link do zdjęcia)
+        //attr - bierze wartość atrybutu (link do zdjęcia)
         var i_path = $(this).attr('src');
 
-        var a = $('#magnify').outerWidth();
-        var b = $('#magnify').outerHeight();
-
         //append - wkleja w koniec 'body' kod
-        $('body').append('<div id="overlay"></div><div id="magnify"><img src="' + i_path + '"><div id="close-popup"><i></i></div></div>');
-        $('#magnify').css({
-            left: ($(document).width() - $('#magnify').outerWidth()) / 2,
-            top: ($(window).height() - $('#magnify').outerHeight()) / 2
+        $('body').append('<div id="wrap-overlay"><div id="overlay"></div><div id="magnify"><img src="' + i_path + '"></div></div>');
 
-            
-
-            //left: $(document).width() / 2,
-            //top: $(window).height() / 2 
-        });
-
-         //fadeIn - wybrane elementy będą niewidoczne fast - szybko
-        $('#overlay, #magnify').fadeIn('fast');
+        //fadeIn - wybrane elementy będą niewidoczne fast - szybko
+        $('#overlay, #magnify, #wrap-overlay').fadeIn('fast');
 
     });
 
-    
-
-    $('body').on('click', '#close-popup, #overlay', function (event) {
+    $('body').on('click', ' #overlay, #wrap-overlay', function (event) {
         event.preventDefault();
 
-        $('#overlay, #magnify').fadeOut('fast', function () {
-            $('#close-popup, #magnify, #overlay').remove();
+        $('#overlay, #magnify, #wrap-overlay').fadeOut('fast', function () {
+            $(' #magnify, #overlay, #wrap-overlay ').remove();
         });
-       
-    });
 
-   
+
+    });  
 });
